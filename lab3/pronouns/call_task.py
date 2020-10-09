6d4a2a5b7153291dc11d1a59fc3e44a087dcccfd
Egemen Yiğit Kömürcü <ekomurcu@sabanciuniv.edu>
2020-10-09 14:00:16 +0200

    Update call_task.py

--- lab3/pronouns/call_task.py
+++ lab3/pronouns/call_task.py
@@ -1,9 +1,6 @@
 from tasks import add
-result= add.delay(4,4)
-#print(result.ready())
-#print(result.get(timeout=1))
-#result.get(propagate=False)
-#result.traceback
+count=0
+counts={"han":0,"hon":0, "den":0, "det":0, "denna":0, "denne":0, "hen":0}
+result= pronoun_count_per_unique_tweet.delay("~/Data/20275995-d9e1-442c-bf33-7979aab6db8b", counts, count)
 for v in result.collect():
     print(v)
-
